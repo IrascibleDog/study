b285adfc415fcb6d6d9f250dcd1103ff632db00a
IrascibleDog <irascible.dog@gmail.com>
2015-03-26 09:51:09 +0200

    head 30%

--- js/scripts.js
+++ js/scripts.js
@@ -14,31 +14,31 @@ $(document).ready(function() {
         console.log(("#lang_selector").parent())
     });
 });
-$(document).ready(function(){
-    var access = prompt("Who came?",'');
-    var password;
-
-    if (access != null){
-        if (access == "admin"){
-            password = prompt("Your password?",'')
-
-            if (password !=null){
-                if (password == "BlackMaster"){
-                    alert("Welcome!");
-                }
-                else{
-                    alert("Wrong password..!");
-                }
-            }
-            else{
-                alert("Access denied!");
-            }
-        }
-        else{
-            alert("I don't know you..!");
-        }
-    }
-    else{
-        alert("Access denied!");
-    }
-});
+//$(document).ready(function(){
+//    var access = prompt("Who came?",'');
+//    var password;
+//
+//    if (access != null){
+//        if (access == "admin"){
+//            password = prompt("Your password?",'')
+//
+//            if (password !=null){
+//                if (password == "BlackMaster"){
+//                    alert("Welcome!");
+//                }
+//                else{
+//                    alert("Wrong password..!");
+//                }
+//            }
+//            else{
+//                alert("Access denied!");
+//            }
+//        }
+//        else{
+//            alert("I don't know you..!");
+//        }
+//    }
+//    else{
+//        alert("Access denied!");
+//    }
+//});
